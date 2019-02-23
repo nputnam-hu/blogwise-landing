3920f5704f9292e5aad65f22a121aad8ea695571
Noah Putnam <noahputnam@gmail.com>
2019-02-23 10:40:28 -0500

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
+import "./styles.css"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div id="index-container">
+    <div style={{ textAlign: "center", paddingTop: "150px" }}>
+      {/* <img
+        src="https://app.blogwise.co/static/media/logo.243ba0ff.png"
+        style={{ height: "60px" }}
+        alt="Blogwise"
+      /> */}
+      <h1>blogwise is coming soon to a universe near you</h1>
+    </div>
+  </div>
+)
