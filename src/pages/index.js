7e8d5750740cc852da26fac1e1110869dddf2d4c
Ivraj Seerha <iseerha@college.harvard.edu>
2019-03-12 16:05:35 -0700

    Pulls out index.sass into sass module

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,18 @@ import { graphql, Link } from 'gatsby'
 import Img from 'gatsby-image'
 import Layout from '../components/Layout'
 import WhyCardGrid from '../components/WhyCardGrid'
-import './index.sass'
+import styles from '../styles/index.module.sass'
 
 const Index = ({ data }) => (
   <Layout>
-    <div id="index-container">
+    <div className={styles.index}>
       <Img
         fluid={data.upperSwoosh.childImageSharp.fluid}
         style={{ position: 'absolute' }}
         alt="Woman with megaphone"
-        className="upperSwoosh"
+        className={styles.upperSwoosh}
       />
-      <div id="sectionone-container">
+      <div className={styles.sectionone}>
         <div style={{ height: '4vh' }} />
         <h1>You made the perfect product.</h1>
         <h1>Time to tell everyone.</h1>
@@ -22,7 +22,7 @@ const Index = ({ data }) => (
         <Img
           fixed={data.mobile.childImageSharp.fixed}
           alt="cloud of blogs"
-          className="mobile-headerPhoto"
+          className={styles.sectionone__mobileHeaderPhoto}
         />
         <p>
           blogwise is the easiest way to create a blog for your business. Sign
@@ -48,37 +48,37 @@ const Index = ({ data }) => (
           Apply For Beta
         </a>
       </div>
-      <div id="sectiontwo-container">
-        <div id="sectiontwo-text">
+      <div className={styles.sectiontwo}>
+        <div className={styles.sectiontwo__text}>
           <h2>Get your message out with a beautiful blog built in minutes</h2>
           <p>Like actually in minutes, no joke</p>
         </div>
         <Img
           fluid={data.screenshot1.childImageSharp.fluid}
           alt="blog demo"
-          className="screenshot1"
+          className={styles.sectiontwo__screenshot1}
         />
-        <p id="template-explanation">
+        <p className={styles.sectiontwo__template}>
           Our templates autopopulate your information - <br /> so you never have
           to struggle with broken customization tools.{' '}
         </p>
         <Img
           fluid={data.screenshot2.childImageSharp.fluid}
           alt="blog demo"
-          className="screenshot2"
+          className={styles.sectiontwo__screenshot2}
         />
       </div>
       <Img
         fluid={data.midSwoosh.childImageSharp.fluid}
         style={{ position: 'absolute' }}
         alt="swoosh"
-        className="midSwoosh"
+        className={styles.midSwoosh}
       />
-      <div id="sectionthree-container">
+      <div className={styles.sectionthree}>
         <h1>WHY BLOGWISE?</h1>
         <WhyCardGrid />
       </div>
-      <div id="sectionfour-container">
+      <div className={styles.sectionfour}>
         <h1>Content Marketing is the Future</h1>
         <h2>Are you ready to blogwise?</h2>
         <br />
