efa3fa9ace0b592d6d65d748fedd2b1c2d016475
Noah Putnam <noahputnam@gmail.com>
2019-05-06 20:15:51 -0400

    add apple support thru mp4

--- src/components/Demo/Demo.js
+++ src/components/Demo/Demo.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react'
 import DemoTab from './DemoTab'
 import styles from './Demo.module.sass'
-import setup from './setup.webm'
-import inspiration from './inspiration.webm'
-import smartTools from './smartTools.webm'
+import setupWM from './setup.webm'
+import setupMP4 from './setup.mp4'
+import inspirationWM from './inspiration.webm'
+import inspirationMP4 from './inspiration.mp4'
+import smartToolsWM from './smartTools.webm'
+import smartToolsMP4 from './smartTools.mp4'
 
-const DemoPresentation = ({ videoUri, title, paragraph1, paragraph2 }) => (
+const DemoPresentation = ({
+  mp4Uri,
+  webmUri,
+  title,
+  paragraph1,
+  paragraph2,
+}) => (
   <div className={styles.presentation}>
     <div style={{ margin: 'auto' }}>
       {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
       <video
         className={styles.presentation__video}
         alt="Demo"
-        src={videoUri}
         autoPlay
         loop
         playsInline
         muted
-      />
+      >
+        <source type="video/webm" src={webmUri} />
+        <source type="video/mp4" src={mp4Uri} />
+      </video>
     </div>
     <div className={styles.presentation__text}>
       <h2 className={styles.presentation__title}>{title}</h2>
@@ -36,7 +47,8 @@ class Demo extends Component {
     const { activeTab } = this.state
     const presentationProps = [
       {
-        videoUri: setup,
+        webmUri: setupWM,
+        mp4Uri: setupMP4,
         title: 'Build your blog in 5 minutes',
         paragraph1:
           "Answer a few questions and we'll be able to set up an initial blog for you. Connect the blog to a domain you own and write a few articles and you'll be on your way to building a loyal audience.",
@@ -44,14 +56,16 @@ class Demo extends Component {
           "Once you've built your initial blog, use our integrated blog editor to customize it fully to your company's brand.",
       },
       {
-        videoUri: inspiration,
+        webmUri: inspirationWM,
+        mp4Uri: inspirationMP4,
         title: 'Tools to Empower Your Creativity',
         paragraph1:
           'We are making content creation streamlined so you can have a great blog and scale your company. Every blog is bundled with tools for generating click-worthy headlines, finding free, professional cover photos, and compiling relevant links from twitter.',
         paragraph2: '',
       },
       {
-        videoUri: smartTools,
+        webmUri: smartToolsWM,
+        mp4Uri: smartToolsMP4,
         title: 'View your changes in real time',
         paragraph1:
           'Manage everything about your blog in one place: connect a custom domain, upload brand assets, and write articles all on our dashboard.',
